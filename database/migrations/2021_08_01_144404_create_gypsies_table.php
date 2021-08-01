d63cec7fad8e2da6a5fe94edf6149c4bb6e311a8
Nicholas Agwah <nagwah@teqbridgeltd.com>
2021-08-01 18:35:14 +0100

    migrations1

---
+++ database/migrations/2021_08_01_144404_create_gypsies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGypsiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('gypsies', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('type');
+            $table->string('base');
+            $table->string('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('gypsies');
+    }
+}
